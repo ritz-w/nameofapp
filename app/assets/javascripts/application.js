861fed6c108359aa29a11a82318354c84ac87538
Ritz-W <ritz.wu@network.rca.ac.uk>
2017-03-08 01:52:42 +0900

    reset javascript files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-//navbar mobile animation styling
-$(document).ready(function () {
-        $(".navbar-toggle").on("click", function () {
-            $(this).toggleClass("active");
-        });
-    });
