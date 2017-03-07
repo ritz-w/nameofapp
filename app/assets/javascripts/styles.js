e700440fc6e8a73898978240ae5287bdabf0cca3
Ritz-W <ritz.wu@network.rca.ac.uk>
2017-03-08 01:14:23 +0900

    fixed responsivity errors

--- app/assets/javascripts/styles.js
+++ app/assets/javascripts/styles.js
@@ -4,3 +4,29 @@ $(document).ready(function () {
             $(this).toggleClass("active");
         });
     });
+
+var shopItems = [
+  {
+    pic: "img/buzzing.png",
+    text: "About Object Text"
+  },
+
+  {
+    pic: "img/studiovisit.jpg",
+    text: "http://cargocollective.com/rcastudios"
+  },
+
+    {
+    pic: "img/studiovisit.jpg",
+    text: "http://cargocollective.com/rcastudios"
+  }
+];
+  
+for (var i = 0; i < shopItems.length; i++) {
+  $(".row object-lineup").appendChild("\
+    <div class='col-md-3'>\
+      <%= image_tag(+ shopItems[i].url + , class: "img-product") %>"\
+      <p class="object-title-text"> + shopItems[i].text + </p>
+      </div>\
+  ");
+}
