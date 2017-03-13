b7ff3ea2c9c5d73f3b381374184ec672e7a4964a
Ritz-W <ritz.wu@network.rca.ac.uk>
2017-03-14 00:08:53 +0900

    changed product list to slick slider, added js, css vendor files

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.regular').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true
+      });
+  });
