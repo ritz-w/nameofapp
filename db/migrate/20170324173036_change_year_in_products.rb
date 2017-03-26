c268908070357462ea940438ba64bbf465549679
Ritz-W <ritz.wu@network.rca.ac.uk>
2017-03-26 22:39:18 +0900

    changed year in products to debug heroku error datatype mismatch

--- db/migrate/20170324173036_change_year_in_products.rb
+++ db/migrate/20170324173036_change_year_in_products.rb
@@ -1,5 +1,9 @@
 class ChangeYearInProducts < ActiveRecord::Migration[5.0]
-  def change
-    change_column :products, :year_made, :date
+  def self.up
+    rename_column :products, :year_made, :year_made_string
+    add_column :products, :year_made, :date
+
+    Product.reset_column_information
+    Product.find_each { |c| c.update_attribute(:year_made, c.year_made_string) } 
+    remove_column :products, :year_made_string
   end
-end
