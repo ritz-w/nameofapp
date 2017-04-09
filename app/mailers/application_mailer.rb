b51a31ca5b4ca6cc9a2d0e88ffc154d2432b1c9a
Ritz-W <ritz.wu@network.rca.ac.uk>
2017-04-09 23:30:26 +0900

    added action mailer, formatted contact form

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'ritz.wu@outlook.com'
-  layout 'mailer'
+
+  def contact_form(email, name, message)
+    @message = message
+    mail(:from => email,
+        :to => 'your-email@example.com',
+        :subject => "A new contact form message from #{name}")
+  end
 end
