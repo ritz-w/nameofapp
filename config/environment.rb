9e00d83901efaa20809f50fee4f5ba50b7698630
Ritz-W <ritz.wu@network.rca.ac.uk>
2017-04-20 13:28:52 +0900

    destroyed sendgrid, edited sendgrid code

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
-  :port => '587',
-  :authentication => :plain,
-  :user_name => ENV['ritz.wu@outlook.com'],
-  :password => ENV['1hatehackers'],
-  :domain => 'heroku.com',
-  :enable_starttls_auto => true
-}
