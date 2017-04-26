14f2cee4ac59c4ea5c04a2e1a2e7c9711c91ea95
Ritz-W <ritz.wu@network.rca.ac.uk>
2017-04-27 00:57:15 +0900

    edited user and application mailers to solve conflicting issue

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,10 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'ritz.wu@outlook.com'
-
-  def contact_form(email, name, message)
-    @message = message
-    mail(:from => email,
-        :to => 'your-email@example.com',
-        :subject => "A new contact form message from #{name}")
-  end
+  default from: "ritz.wu@outlook.com"
+  layout 'mailer'
 end
