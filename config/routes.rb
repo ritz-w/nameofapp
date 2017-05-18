a049f28edaf5d425a76fe28e33a63c15439e7ace
Ritz-W <ritz.wu@network.rca.ac.uk>
2017-05-18 14:16:25 +0900

    added stripe, added payments controller, added payment button

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => { :registrations => "user_registrations" }
   resources :users
  
+  
+  post 'payments/create' => 'payments#create'
+  get 'payments/create' => 'payments#create'
+
+
   resources :products do
     resources :comments
     get '/products/:id', to: 'products#show', as: 'product'
--- spec/factories/product_factory.rb
+++ spec/factories/product_factory.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     name "generic painting"
   end
 
-    factory :comment, class: Comment do
+  factory :comment, class: Comment do
     rating 3
     user
     association :product_id, factory: :product1
